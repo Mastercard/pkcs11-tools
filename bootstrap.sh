7f39b4182729cc2f63409605e7812bef911f0636
Eric Devolder <eric.devolder@mastercard.com>
2020-07-07 12:10:17 +0200

    adding realloc() and calloc() to the list of GNUlib modules

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 git submodule update --init .gnulib
 
 # invoke gnulib
-.gnulib/gnulib-tool --import --dir=. --lib=libgnu --source-base=gl --m4-base=m4 --doc-base=doc --tests-base=tests --aux-dir=. --no-conditional-dependencies --no-libtool --macro-prefix=gl byteswap gethostname getline getopt-gnu malloc-gnu regex strcase termios time
+.gnulib/gnulib-tool --import --dir=. --lib=libgnu --source-base=gl --m4-base=m4 --doc-base=doc --tests-base=tests --aux-dir=. --no-conditional-dependencies --no-libtool --macro-prefix=gl byteswap gethostname getline getopt-gnu malloc-gnu calloc-gnu realloc-gnu regex strcase termios time
 
 # create configure scripts
 autoreconf -vfi
